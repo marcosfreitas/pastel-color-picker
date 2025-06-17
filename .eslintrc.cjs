8a101b0792ac2a7a40df06d0d9cc230462acfb84
Chain34 <chain34.wvasr@passmail.net>
2025-06-17 09:33:31 +0100

    Update ESLint configuration and dependencies; switch to plugin-based TypeScript ESLint setup and upgrade to version 7.18.0 in package.json and package-lock.json

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,19 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
